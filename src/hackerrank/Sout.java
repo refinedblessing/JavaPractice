9d41cf2d764a5ad9670e6352df93b1123858215a
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2023-09-19 20:50:47 -0700

    Update class name

--- src/hackerrank/Sout.java
+++ src/hackerrank/Sout.java
@@ -3,7 +3,7 @@ package hackerrank;
 // Problem Link
 // https://www.hackerrank.com/challenges/welcome-to-java/problem
 
-class Solution {
+class Sout {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
