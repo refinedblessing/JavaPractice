e4ed41386ea80226f43d83eea651239aa2467580
Blessing Edugie Ebowe <blessingebowe@gmail.com>
2023-09-20 07:30:12 -0700

    Hackerrank java if else challenge

--- src/hackerrank/IfElse.java
+++ src/hackerrank/IfElse.java
@@ -1,2 +1,32 @@
-package hackerrank;public class IfElse {
+package hackerrank;
+
+// Problem Link
+// https://www.hackerrank.com/challenges/java-if-else/problem
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class IfElse {
+    public static void main(String[] args) throws IOException, IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        if ((N % 2) == 0) {
+            if (N <= 5) printNotWeird();
+            else if (N <= 20) printWeird();
+            else printNotWeird();
+        }
+        else printWeird();
+
+        bufferedReader.close();
+    }
+
+    static void printWeird() {
+        System.out.println("Weird");
+    }
+
+    static void printNotWeird() {
+        System.out.println("Not Weird");
+    }
 }
